f33e948b0e63036e1b6ab807bed6030af149cb49
techno-tanoC <develop.tanoc@gmail.com>
2017-10-08 17:46:22 +0900

    add renamer

--- lib/black_sky/renamer.rb
+++ lib/black_sky/renamer.rb
@@ -1,4 +1,46 @@
 module BlackSky
   class Renamer
+    def initialize(dest)
+      @dest, @mutex = dest, Mutex.new
+    end
+
+    def rename(from, name)
+      sync do
+        fresh = fresh_name(name)
+        #todo exception
+        File.rename(from, fresh)
+      end
+    end
+
+    def copy(from, name)
+      sync do
+        fresh = fresh_name(@dest, name)
+        FileUtils.copy(from, fresh)
+        File.chown(1000, 1000, fresh)
+        File.chmod(0600, fresh)
+      end
+    end
+
+    private
+    def fresh_name(name, ext = "mp4", count = 0)
+      name = new_name(name, ext, count)
+      if File.exist?(name)
+        fresh_name(name, ext, count + 1)
+      else
+        name
+      end
+    end
+
+    def new_name(name, ext, count)
+      if count.zero?
+        File.join(@dest, "#{name}.#{ext}")
+      else
+        File.join(@dest, "#{name}(#{count}).#{ext}")
+      end
+    end
+
+    def sync(&block)
+      @mutex.synchronize(&block)
+    end
   end
 end
